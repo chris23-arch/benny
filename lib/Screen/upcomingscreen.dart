df685bfb3437610f6ef76cec2372f7d2c47aead7
chris23-arch <chukwumitchell29@gmail.com>
2024-06-27 16:48:17 -0700

    add const

--- lib/Screen/upcomingscreen.dart
+++ lib/Screen/upcomingscreen.dart
@@ -799,7 +799,7 @@ class _UpcomingScreenState extends State<UpcomingScreen> {
                             ),
                             textAlign: TextAlign.start,
                           ),
-                           SizedBox(height: 12),
+                           const SizedBox(height: 12),
                             Stack(
                             children: [
                               // Underline
